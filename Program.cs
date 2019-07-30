fa7cbb929e5d2be3678dd5fab600e3f60cc9ad46
jpalamos <jpm.electronica@gmail.com>
2019-07-29 21:17:31 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = Convert.ToInt32(Console.ReadLine());
+            string st ="";
+            if (n % 2 == 0) st = "Par";
+            else st = "Impar";
+
+            Console.WriteLine("El numero ingresado " + n + " es " + st);
+            Console.ReadLine();
+            
+        }
+    }
+}
